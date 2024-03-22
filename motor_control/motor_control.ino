39b704728a915ed938820fcc5b597e8148f608db
Maja Komorowska <majakom863@gmail.com>
2024-03-22 20:15:18 +0100

    Added H-bridge

--- motor_control/motor_control.ino
+++ motor_control/motor_control.ino
@@ -4,11 +4,16 @@ const int pushButton = 2;
 
 // the transistor which controls the motor will be attached to digital pin 9
 const int motorControl = 9;
+const int motorControlR = 10;
 
 const int trigPin = 3;
 const int echoPin = 4;
 int distance, duration;
 
+int distVector[5] = {1000,1000,1000,1000,1000};
+int index = 0;
+bool stop = false;
+bool running = false;
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -17,6 +22,7 @@ void setup() {
 
   // make the transistor's pin an output:
   pinMode(motorControl, OUTPUT);  
+  pinMode(motorControlR, OUTPUT);
   
   //defining trigger pin in the distance detector
   pinMode(trigPin, OUTPUT);
@@ -39,21 +45,42 @@ void loop() {
   // read the state of the button and check if it is pressed
   if(digitalRead(pushButton) == HIGH){
     // ramp up the motor speed
-    for(int x = 0; x <= 255; x++){
-      analogWrite(motorControl, x);
-      delay(50);
-    }
-
+    //for(int x = 0; x <= 255; x+=10){
+      analogWrite(motorControl, 255);
+      delay(150);
+      
+    //}
+    running = true;
     // ramp down the motor speed
     
    
   }
-  else {
-    analogWrite(motorControl, 0);
-    delay(50);
+  delay(1);        // delay in between reads for stability
+  stop = false;
+  for(int a = 0; a < 5; a++) {
+    if(distVector[a] > 10) {
+      stop = false;
+      break;
+    } else {
+      stop = true;
+    }
   }
 
-  delay(1);        // delay in between reads for stability
+  if(stop && running) {
+    analogWrite(motorControl,0);
+    delay(100);
+    for(int x = 0; x <= 255; x+=10){
+      analogWrite(motorControlR, 255);
+      delay(50);
+    }
+    delay(100);
+    analogWrite(motorControlR,0);
+    running = false;
+  }
+
+
+  delay(1);
+
 }
 
 
@@ -70,5 +97,13 @@ int measureDistance() {
   Serial.print(
       distance); // Print the output in serial monitor
   Serial.println(" cm");
+
+  if(index == 5) {
+    index = 0;
+  }
+
+  distVector[index++] = distance;
+
+
   return distance;
 }
